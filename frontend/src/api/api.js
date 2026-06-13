ff5cae97d6b226058774132669705e6c5ab21c76
Tushya <tushya0208@gmail.com>
2026-06-13 12:48:16 +0530

    Added environment variable support for API URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,20 +1,19 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:5000",
 });
 
 API.interceptors.request.use(
   (config) => {
-
     const token =
       localStorage.getItem("token");
 
     if (token) {
-
       config.headers.Authorization =
         `Bearer ${token}`;
-
     }
 
     return config;
